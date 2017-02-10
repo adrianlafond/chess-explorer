eb01b374606dc2bb2535e3ca4aba740f7daffb06
Adrian Lafond <mail@adrianlafond.com>
2017-02-09 22:50:36 -0500

    add pieces unicode constants

---
+++ src/constants/pieces.js
@@ -0,0 +1,13 @@
+// @see https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
+export const WHITE_KING   = '&#9812';
+export const WHITE_QUEEN  = '&#9813';
+export const WHITE_ROOK   = '&#9814';
+export const WHITE_BISHOP = '&#9815';
+export const WHITE_KNIGHT = '&#9816';
+export const WHITE_PAWN   = '&#9817';
+export const BLACK_KING   = '&#9818';
+export const BLACK_QUEEN  = '&#9819';
+export const BLACK_ROOK   = '&#9820';
+export const BLACK_BISHOP = '&#9821';
+export const BLACK_KNIGHT = '&#9822';
+export const BLACK_PAWN   = '&#9823';
