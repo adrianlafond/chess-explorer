15875ad4c6b6ec7715342c02aef0a14b46783398
Adrian Lafond <mail@adrianlafond.com>
2017-02-10 20:21:26 -0500

    render pieces via css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import * as PIECES from './constants/pieces';
 import './App.css';
+import './components/board/pieces.css';
 
 class App extends Component {
   render() {
@@ -13,8 +15,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.renderPieces()}
       </div>
     );
+  }
+
+  renderPieces() {
+    const pieces = Object.keys(PIECES).map((key, index) => {
+      return <span key={index} className={PIECES[key]} />;
+    });
+    return (<p>{pieces}</p>);
   }
 }
 
